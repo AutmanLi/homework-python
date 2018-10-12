b6b062e494b5795b73bc9031c37050579bb03b72
AutmanLi <autmanli123@gmail.com>
2018-10-12 22:45:25 +0800

    素数方法
    增加了素数方法的注释

--- homework-1012/sushu.py
+++ homework-1012/sushu.py
@@ -12,11 +12,15 @@ import math
 
 # 验证该数是不是素数，只需要验证该数能够整除从2到他的平方根即可
 def check_sushu(num):
-        sqrt=math.floor(math.sqrt(num))+1
-        for i in range(2,sqrt):
-            if num % i == 0:
-                return False
-        return True
+    # 求该数的平方根
+    sqrt = math.floor(math.sqrt(num)) + 1
+    # 循环从2到平方根的数
+    for i in range(2, sqrt):
+        # 如果该数能够整除，说明该数不是素数
+        if num % i == 0:
+            return False
+    return True
+
 
 #filter用法，过滤掉数组中返回值为false的项
 sushu_arr=filter(check_sushu,range(1,1000000))
