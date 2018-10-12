6e0fd957c1a4e9a1236ea823c42db504c5c63833
AutmanLi <autmanli123@gmail.com>
2018-10-12 10:18:28 +0800

    Initial commit
    提交

---
+++ class_learn.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@version: ??
+@author: autmanli
+@license: Apache Licence 
+@file: class_learn.py
+@time: 2018/10/12 10:04
+"""
+
+class A(object):
+    pass
+
+class B(A):
+    pass
+
+class C(B):
+    pass
+
+class D(B):
+    pass
+
+class E(C,D):
+    pass
